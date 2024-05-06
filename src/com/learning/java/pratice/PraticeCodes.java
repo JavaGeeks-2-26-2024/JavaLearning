2042255e1edea35785611279d08e29d48e6fef77
HemanthChirikonda <67725821+HemanthChirikonda@users.noreply.github.com>
2024-05-06 14:39:30 -0400

    files added

---
+++ src/com/learning/java/pratice/PraticeCodes.java
@@ -0,0 +1,18 @@
+package com.learning.java.pratice;
+
+public class PraticeCodes {
+
+	public static void main(String[] args) {
+		 int i = 1;
+		 while (i<=50){
+			 System.out.println(i); 
+		     if (i % 2 == 0) {
+				 System.out.println("even");
+			 }else{
+				 System.out.println("odd");
+			 } 
+			 i++;
+		 }
+	}
+
+}
